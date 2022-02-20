d102e6c6eb281a9294536d65a779e5044786b152
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-02-20 23:47:59 +0530

    Added FIREBASE FOR BACKEND , USING ASYNC CODE IN COMPONENT

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
-function App() {
+const App = () => {
   const showCart = useSelector((state) => state.toggle.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  //* async code in component for reducer
+  useEffect(() => {
+    fetch("https://redux-cartbackend-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
       <Products />
     </Layout>
   );
-}
+};
 
 export default App;
